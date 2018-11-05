c4f3c7b658a277556c685ac10db79492fdef1839
Andrej Oravkin <aoravkin@gmail.com>
2018-11-05 23:13:45 +0000

    Add show repos functionality

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = "0b4f54c65bec74c72a45";
     this.client_secret = "931786b57a97f8feaf640587f64515326a224511";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   // Get user method
@@ -12,10 +14,21 @@ class GitHub {
       }&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
     const profile = await profileResponse.json();
 
+    const repos = await repoResponse.json();
+
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
